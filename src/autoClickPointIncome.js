60dd7ff2d12835bcaefcb4e14b48c16b3fae41c3
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-02-27 17:57:06 +0900

    Fix the coding style

--- src/autoClickPointIncome.js
+++ src/autoClickPointIncome.js
@@ -1,4 +1,4 @@
-function autoClickPointIncome() {
+function autoClickPointIncome () {
   const threads = GmailApp.search('ポイントインカム クリック from:mag@pointi.jp');
 
   threads.forEach(thread => {
@@ -13,24 +13,23 @@ function autoClickPointIncome() {
   });
 }
 
-function pickUrlsFromMessageBody(messageBody) {
+function pickUrlsFromMessageBody (messageBody) {
   const pointLines = messageBody.match(/クリックで(.+)ptゲット(\r\n|\n|\r)https(.+)(\r\n|\n|\r)/ig);
 
   if (pointLines === null) {
-    return []
+    return [];
   }
 
   return pointLines.map(line => {
-    return line.match(/(\r\n|\n|\r)(.+)(\r\n|\n|\r)/)[2]
-  })
+    return line.match(/(\r\n|\n|\r)(.+)(\r\n|\n|\r)/)[2];
+  });
 }
 
-function clickUrl(url) {
+function clickUrl (url) {
   Logger.log(`Click : ${url}`);
   try {
     return UrlFetchApp.fetch(url);
-  }
-  catch (e) {
+  } catch (e) {
     console.log(e);
     Logger.log(e);
     return e;
